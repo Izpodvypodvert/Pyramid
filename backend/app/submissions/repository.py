f0d4b56308c09a79aa54a0a11bb2510627b7a800
Izpodvypodvert <albukerke87@mail.ru>
2024-02-22 21:41:35 +0300

    added get_user_submissions_by_step_id for submission service

--- backend/app/submissions/repository.py
+++ backend/app/submissions/repository.py
@@ -1,6 +1,18 @@
+from sqlmodel import select
 from app.submissions.models import Submission
 from app.utils.repository import SQLModelRepository
+from backend.app.users.models import User
 
 
 class SubmissionRepository(SQLModelRepository):
     model = Submission
+
+    async def get_user_submissions_by_step_id(
+        self, step_id: int, user: User
+    ) -> list[Submission]:
+        statement = select(self.model).where(
+            self.model.step_id == step_id, self.model.student_id == user.id
+        )
+        submissions = await self.session.exec(statement)
+
+        return submissions.all()
--- backend/app/submissions/service.py
+++ backend/app/submissions/service.py
@@ -1,4 +1,8 @@
 from app.utils.service import BaseService
+from app.users.models import User
 
 
-class SubmissionsService(BaseService): ...
+class SubmissionsService(BaseService):
+    async def get_user_submissions_by_step_id(self, step_id: int, user: User):
+        async with self.transaction_manager:
+            return await self.repository.get_user_submissions_by_step_id(step_id, user)
