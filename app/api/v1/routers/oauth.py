d44408890ef44d567bedebfd5517e8f8782266c2
Alex <izpodvypodwerta@gmail.com>
2025-06-26 22:13:47 +0300

    fixed bug in auth_google_callback

--- app/api/v1/routers/oauth.py
+++ app/api/v1/routers/oauth.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Depends, Request
 from fastapi.responses import RedirectResponse
-from app.utils.logger import oauth_logger
+
 from app.core.config import settings
 from app.users.auth_config import get_user_manager
 from app.users.manager import UserManager
 from app.users.oauth_config import oauth
-from app.users.service import generate_access_token, get_google_user_info, get_or_create_user
-
+from app.users.service import (
+    generate_access_token,
+    get_google_user_info,
+    get_or_create_user,
+)
+from app.utils.logger import oauth_logger
 
 router = APIRouter(tags=["OAuth"])
 
@@ -15,13 +19,13 @@ router = APIRouter(tags=["OAuth"])
 async def auth_google_callback(
     request: Request,
     user_manager: UserManager = Depends(get_user_manager),
-    oauth_client = Depends(lambda: oauth)
+    oauth_client=Depends(lambda: oauth),
 ):
     try:
         user_info = await get_google_user_info(request, oauth_client)
         user = await get_or_create_user(user_info, user_manager)
         access_token = await generate_access_token(user)
-        redirect_url = f"{settings.frontend_oauth_redirect_url}={access_token}"
+        redirect_url = f"{settings.frontend_oauth_redirect_url}?token={access_token}"
         return RedirectResponse(redirect_url)
     except Exception as e:
         oauth_logger.error(str(e))
@@ -29,7 +33,7 @@ async def auth_google_callback(
 
 
 @router.get("/auth/google/login")
-async def google_login(request: Request, oauth_client = Depends(lambda: oauth)):
+async def google_login(request: Request, oauth_client=Depends(lambda: oauth)):
     try:
         redirect_uri = request.url_for("auth_google_callback")
         return await oauth_client.google.authorize_redirect(request, redirect_uri)
